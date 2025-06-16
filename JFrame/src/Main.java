278948493b0010056f53077ec896d6b55f398d71
Amaliamelo <amalia.melo@usp.br>
2025-06-16 16:19:23 -0300

    atualizando

---
+++ JFrame/src/Main.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompany.designpatternsproject;
+
+import javax.swing.SwingUtilities;
+
+
+public class Main {
+
+		public static void main(String[] args) {
+		       new JWelcome();
+		    
+		}
+
+}
